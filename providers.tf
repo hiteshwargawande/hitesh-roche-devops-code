bc9c08601d3ada97f74c276f8773fc1dbf315ff3
hiteshwargawande <hiteshwargawande@gmail.com>
2025-11-05 06:10:22 +0000

    updated S3 bucket+Dynamodb table

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket         = "hitesh-roche-terraform-bucket"
+    key            = "dev/hitesh/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "hitesh-roche-table1" 
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
